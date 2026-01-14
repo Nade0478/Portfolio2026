a47e6428bcf6d8426fb39dfce68c352e06708a45
Nade04 <86410129+Nade0478@users.noreply.github.com>
2026-01-14 09:09:52 +0100

    commit du 14 01 26 suite

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Controllers\CompetenceController;
 use App\Http\Controllers\CvController;
 use App\Http\Controllers\SkillController;
 use Illuminate\Support\Facades\Route;
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -12,3 +14,8 @@ Route::get('/', function () {
 Route::get('/skills', [SkillController::class, 'index']);
 Route::get('/skills/{id}', [SkillController::class, 'show']);
 Route::get('/cv', [CvController::class, 'show']);
+Route::apiResource('competences', CompetenceController::class);
+Route::apiResource('skills', SkillController::class);
+Route::apiResource('cvs', CvController::class);
+Route::apiResource('competences', CompetenceController::class);
+Route::apiResource('skills', SkillController::class);
